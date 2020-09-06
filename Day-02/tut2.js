1007840a755fa182ae8645e7e6b87477824a951a
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-06 20:44:48 +0530

    first commit

---
+++ Day-02/tut2.js
@@ -0,0 +1,13 @@
+ console.time('your code took');
+ console.log('hello console');
+ console.log(2+5);
+ console.log(6+8);
+ console.log(35);
+ console.log(true);
+ console.log({harry:'this',marks:34});
+ console.table({harry:'this',marks:34});
+console.warn('this is the warnning');
+//console.clear();
+console.timeEnd('your code took');
+console.assert('age>189, age >189 is not possible');
+console.error('this in the error');
