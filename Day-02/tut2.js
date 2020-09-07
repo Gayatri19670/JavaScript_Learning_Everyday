bfb58096bd252a0ca2b4c83bbf21ed53dfaa0ddb
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-07 12:19:33 +0530

    first commit

--- Day-02/tut2.js
+++ Day-02/tut2.js
@@ -1,4 +1,9 @@
- console.time('your code took');
+ 
+ console.log('wlcm to tut 2');
+ 
+//  types of console
+ 
+ 
  console.log('hello console');
  console.log(2+5);
  console.log(6+8);
@@ -6,8 +11,10 @@
  console.log(true);
  console.log({harry:'this',marks:34});
  console.table({harry:'this',marks:34});
-console.warn('this is the warnning');
+ console.warn('this is the warnning');
 //console.clear();
-console.timeEnd('your code took');
-console.assert('age>189, age >189 is not possible');
-console.error('this in the error');
+ console.time('your code took');
+// your code
+ console.timeEnd('your code took');
+ console.assert('age>189, age >189 is not possible');
+ console.error('this in the error');
---
+++ Day-03/tut3.js
@@ -0,0 +1,45 @@
+console.log('tut3');
+// Variables in js
+// var, let, const
+var name = 'harry';
+var channel;
+var marks = 3454;
+marks = 0;
+channel = 'CodeWithHarry'
+console.log(name, channel, marks);
+// Rules for creating JavaScript Variables
+/*
+1. Cannot start with numbers
+2. Can start with letter, numbers, _ or $
+3. Are case sensitive
+*/
+var city = 'Delhi';
+console.log(city);
+
+const ownersName = 'Hari Ram';
+// ownersName = 'Harry'; // Cannot do this (error)
+console.log(ownersName);
+const fruit = 'Orange';
+
+{
+ let city = 'Rampur';   
+ city = 'Kolkata';
+ console.log(city);
+}
+console.log(city);
+
+
+const arr1 = [12,23,12,53, 3]; 
+// arr1.push(45);
+console.log(arr1)
+
+/* Most common programming case types:
+
+1. camelCase  
+2. kebab-case
+3. snake_case 
+4. PascalCase 
+
+
+*/
+   
