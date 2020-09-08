d776713cf946fae2a7d026414d287b7a32826154
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-08 10:46:20 +0530

    first commit

--- Day-02/tut2.js
+++ Day-02/tut2.js
@@ -1,20 +1,26 @@
- 
- console.log('wlcm to tut 2');
- 
+
+console.log('wlcm to tut 2');
+
 //  types of console
- 
- 
- console.log('hello console');
- console.log(2+5);
- console.log(6+8);
- console.log(35);
- console.log(true);
- console.log({harry:'this',marks:34});
- console.table({harry:'this',marks:34});
- console.warn('this is the warnning');
+
+console.log('hello console');
+console.log(2 + 5);
+console.log(6 + 8);
+console.log(35);
+console.log(true);
+console.log({ Samir: 'this', marks: 34 });
+console.table({ Samir: 'this', marks: 34 });
+console.warn('this is the warnning');
 //console.clear();
- console.time('your code took');
+console.time('your code took');
 // your code
- console.timeEnd('your code took');
- console.assert('age>189, age >189 is not possible');
- console.error('this in the error');
+console.timeEnd('your code took');
+console.assert('age>189, age >189 is not possible');
+console.error('this in the error');
+
+// Single line comment
+
+/*
+multi line
+comment 
+*/
---
+++ Day-04/tut4.js
@@ -0,0 +1,49 @@
+console.log('hello! world Welcome to JavaScript');
+
+// Data Types In JavaScript
+
+// Primitive data types
+
+// String
+let name = "harry";
+console.log("My string is " + name);
+console.log("Data type is " + (typeof name));
+
+// Numbers
+let marks = 34.4;
+console.log("Data type is " + (typeof marks));
+
+// Boolean
+let isDriver = true;
+console.log("Data type is " + (typeof isDriver));
+
+// Null
+let nullVar = null;
+console.log("Data type is " + (typeof nullVar));
+
+// Undefined
+let undef = undefined;
+console.log("Data type is " + (typeof undef));
+
+// Reference Data Types
+
+// Arrays
+let myarr = [1, 2, 3, 4, false, "string"];
+console.log("Data type is " + (typeof myarr));
+
+// Object Literals
+let stMarks = {
+    harry: 89,
+    Shubham: 36,
+    Rohan: 34
+}
+console.log( typeof stMarks);
+
+function findName() {
+    
+}
+console.log( typeof findName);
+
+let date = new Date();
+console.log( typeof date);
+
