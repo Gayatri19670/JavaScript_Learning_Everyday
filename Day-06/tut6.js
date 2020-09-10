ec0e86a3f45cdd54f9aec5b8f51162b551f89151
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-10 17:13:22 +0530

    first commit

--- Day-06/tut6.js
+++ Day-06/tut6.js
@@ -1,4 +1,4 @@
-console.log('JavaScript_Learning_Day-05');
+console.log('JavaScript_Learning_Day-06');
 const myName = 'Samir';
 const greeting = 'Good Morning';
 console.log(greeting + ' ' + myName);
