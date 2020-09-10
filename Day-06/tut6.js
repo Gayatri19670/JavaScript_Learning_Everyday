1ed1a239ebe6e32a3e988ae5982564200ab42f00
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-10 08:39:25 +0530

    first commit

---
+++ Day-06/tut6.js
@@ -0,0 +1,10 @@
+console.log('We are at tut6');
+const myName = 'Samir';
+const greeting = 'Good Morning';
+ console.log(greeting+' '+myName);
+
+ let html;
+ html = "<h1> hello i am samir gupta</h1>" +
+        "<p>what is your name</P>";
+        
+ console.log(html);
