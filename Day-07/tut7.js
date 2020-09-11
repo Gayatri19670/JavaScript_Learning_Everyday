1ce40fcf4c5f6d159d46b14ef419539d007ab522
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-11 14:31:57 +0530

    first commit

--- Day-07/tut7.js
+++ Day-07/tut7.js
@@ -21,45 +21,40 @@ console.log(arr.length);
 
 
 //   for check a array is a array or not
-// console.log(Array.isArray(arr));
-// console.log(Array.isArray(array1);
+console.log(Array.isArray(arr));
+console.log(Array.isArray(array1);
 
-// for incerting an element into a array
+        // for incerting an element into a array
 
-arr[0] = 'Samir';
-let arrelement = arr[0];
-console.log('element :', arrelement);
-console.log(arr);
+        arr[0] = 'Samir';
+        let arrelement = arr[0]; console.log('element :', arrelement); console.log(arr);
 
 
-// index of element of an array
-let value = marks.indexOf(73);
-console.log(value);
+        // index of element of an array
+        let value = marks.indexOf(73); console.log(value);
 
-// Mutating or Modifying arrays
+        // Mutating or Modifying arrays
+        //  push the element from the last of the array
+        marks.push(3534); console.log(marks);
+        // push the element in the first of the array
+        marks.unshift(1222); console.log(marks);
+        // pop the element from the last of the array
+        marks.pop(); console.log(marks);
+        // push the element in the start of the array
+        marks.shift(); console.log(marks);
+        // for output from to 
+        marks.splice(2, 3); console.log(marks);
+        // for reversing the array
+        marks.reverse(); console.log(marks);
 
+        // for concat 2 arrays
 
-//  push the element from the last of the array
-marks.push(3534);
+        let marks2 = [1, 2, 3, 74, 5]; marks = marks.concat(marks2); console.log(marks);
 
-// push the element in the first of the array
-marks.unshift(1222);
-
-// pop the element from the last of the array
-marks.pop();
-
-// push the element in the start of the array
-marks.shift();
-
-// for output from to 
-marks.splice(2, 3);
-
-// for reversing the array
-marks.reverse();
-console.log(marks);
-
-// for concat 2 arrays
-
-let marks2 = [1, 2, 3, 74, 5];
-marks = marks.concat(marks2);
-console.log(marks);
+        let myobj = {
+            name: 'samir',
+            channel: 'CodewithHarry',
+            isActive: true,
+            marks: [1, 5, 3, 6]
+        }
+        console.log(myobj); console.log(myobj.isActive);
