32e481145047e001e0d5d8b24212af2e4831bd6e
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-13 07:55:58 +0530

    first commit

--- Day-09/tut9.js
+++ Day-09/tut9.js
@@ -1,1 +1,54 @@
 console.log('JavaScript_Learning_Day-09');
+
+console.log(1)
+console.log(2)
+console.log(3)
+
+// General Loops: for,while, do-while
+
+let a = 34;
+a += 1;
+a++;
+console.log(a);
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+}
+
+let j = 110;
+while (j < 10) {
+    console.log(j + 1);
+    j += 1;
+}
+
+let k = 0;
+do {
+    if (k === 5) {
+        k += 1;
+        continue;
+    }
+    console.log(k + 1);
+    k += 1;
+} while (k < 10);
+
+let arr = [2, 5, 6, 4, 2, 3];
+
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element)
+}
+
+arr.forEach(function(element, index, array) {
+    console.log(element, index, array);
+});
+
+let obj = {
+    name: "Rohan Das",
+    age: 78,
+    type: "Dangerous Programmer",
+    os: "Ubuntu"
+}
+for (let key in obj) {
+    console.log(`The ${key} of object is ${obj[key]}`)
+}
+
+console.log('done');
