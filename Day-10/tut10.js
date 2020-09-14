03792abb7860ef9a17e34b324cbab02ee3435879
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-14 17:09:58 +0530

    first commit

--- Day-10/tut10.js
+++ Day-10/tut10.js
@@ -16,32 +16,32 @@ function ui(name) {
 }
 
 console.log(ui("Samir"), i)
-    // const mygreet = function(name, thank='Thank You'){
-    //     let msg = `Happy Birthday ${name} How I wish I could fly to you right now and be with you on this special day of yours. But remember, my good wishes are always there with you. ${thank}!`;
-    //     return msg;
+const mygreet = function(name, thank = 'Thank You') {
+    let msg = `Happy Birthday ${name} How I wish I could fly to you right now and be with you on this special day of yours. But remember, my good wishes are always there with you. ${thank}!`;
+    return msg;
 
-// }
-
-
-// let name = 'Alok';
-// let name2 ='Manjay';
-
-// let val = mygreet(name, 'Thanks a lot');
-// console.log(val);
+}
 
 
-// const myobj = {
-//     name: "SkillF",
-//     game: function(){
-//         return "GTA";
-//     }
-// }
-// console.log(myobj.game())
+let name = 'Alok';
+let name2 = 'Manjay';
 
-// arr = ['fruit', 'vegetable', 'furniture'];
+let val = mygreet(name, 'Thanks a lot');
+console.log(val);
 
-// arr.forEach(function(element, index, array) {
-//     console.log(element, index)
-// });         
+
+const myobj = {
+    name: "SkillF",
+    game: function() {
+        return "GTA";
+    }
+}
+console.log(myobj.game())
+
+arr = ['fruit', 'vegetable', 'furniture'];
+
+arr.forEach(function(element, index, array) {
+    console.log(element, index)
+});
 
 console.log('Happy_Learning_JavaScript');
