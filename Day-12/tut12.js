ba4cea12816465a68dc632be64219dc7399aee24
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-16 06:33:12 +0530

    first commit

---
+++ Day-12/tut12.js
@@ -0,0 +1,36 @@
+console.log('JavaScript_Learning_Day-12');
+
+let a = document;
+a = document.all;
+console.log(a);
+a = document.body;
+console.log(a);
+a = document.forms[0];
+Array.from(a).forEach(function(element) {
+    console.log(element);
+})
+a = document.links[0];
+console.log(a);
+
+/*
+ use document.images and document.
+scripts and print the list of 
+images and scripts on an html page 
+*/
+
+a = document.images;
+console.log(a);
+a = document.scripts;
+console.log(a);
+a = document.links;
+console.log(a);
+
+a = document.all;
+a.forEach(function(element) {
+    console.log(element);
+})
+console.log(a);
+
+array.from(a).forEach(function(element) {
+    console.log(element);
+});
