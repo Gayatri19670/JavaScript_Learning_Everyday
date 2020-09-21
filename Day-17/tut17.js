048ffaa4b7a2a7d6029ffb21f386eae913cd3bd1
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-21 14:46:41 +0530

    first commit

--- Day-17/tut17.js
+++ Day-17/tut17.js
@@ -1,31 +1,23 @@
-console.log('this is tut 16');
-let element = document.createElement('li');
-let text = document.createTextNode('I am a text node');
-element.appendChild(text)
+console.log('JavaScript_Learning_Day-17');
 
-// Add a class name to the li element
-element.className = 'childul';
-element.id = 'createdLi';
-element.setAttribute('title', 'mytitle');
-// element.innerText = '<b>Hello this is created by harry</b>';
-// element.innerHTML = '<b>Hello this is created by harry</b>';
+// document.getElementById("heading").addEventListener("click") 
+// document.getElementById("heading").addEventListener("click", function(){
+//     console.log("You have clicked the heading");
 
-let ul = document.querySelector('ul.this');
-ul.appendChild(element);
-// console.log(ul)
-// console.log(element)
+// }
 
-let elem2 = document.createElement('h3');
-elem2.id = 'elem2';
-elem2.className = 'elem2';
-let tnode = document.createTextNode('This is a created node for elem2');
-elem2.appendChild(tnode);
+document.getElementById("heading").addEventListener("click", function(e) {
+    let variable;
+    console.log("You have clicked the heading");
+    variable = e.target;
+    variable = e.target.className;
+    variable = Array.from(e.target.classList);
 
-element.replaceWith(elem2);
-let myul = document.getElementById('myul');
-myul.replaceChild(element, document.getElementById('fui'));
-myul.removeChild(document.getElementById('lui'));
-let pr = elem2.hasAttribute('href');
-elem2.removeAttribute('id');
-elem2.setAttribute('title', 'myelem2title');
-console.log(elem2, pr);
+    variable = e.target.id;
+    variable = e.offsetX;
+    variable = e.offsetY;
+    variable = e.clientX;
+    variable = e.clientY;
+    console.log(variable);
+    // location.href = '//facebook.com'
+});
