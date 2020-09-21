9bc66e359f1808e0f39f45541c8d8762c6096b55
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-21 14:30:24 +0530

    first commit

---
+++ Day-17/tut17.js
@@ -0,0 +1,31 @@
+console.log('this is tut 16');
+let element = document.createElement('li');
+let text = document.createTextNode('I am a text node');
+element.appendChild(text)
+
+// Add a class name to the li element
+element.className = 'childul';
+element.id = 'createdLi';
+element.setAttribute('title', 'mytitle');
+// element.innerText = '<b>Hello this is created by harry</b>';
+// element.innerHTML = '<b>Hello this is created by harry</b>';
+
+let ul = document.querySelector('ul.this');
+ul.appendChild(element);
+// console.log(ul)
+// console.log(element)
+
+let elem2 = document.createElement('h3');
+elem2.id = 'elem2';
+elem2.className = 'elem2';
+let tnode = document.createTextNode('This is a created node for elem2');
+elem2.appendChild(tnode);
+
+element.replaceWith(elem2);
+let myul = document.getElementById('myul');
+myul.replaceChild(element, document.getElementById('fui'));
+myul.removeChild(document.getElementById('lui'));
+let pr = elem2.hasAttribute('href');
+elem2.removeAttribute('id');
+elem2.setAttribute('title', 'myelem2title');
+console.log(elem2, pr);
