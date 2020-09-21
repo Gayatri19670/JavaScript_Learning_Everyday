1464000fc22d74bbfa8d91cb04a8dfc891b9d99a
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-21 15:15:52 +0530

    first commit

--- Day-17/tut17.js
+++ Day-17/tut17.js
@@ -1,10 +1,20 @@
 console.log('JavaScript_Learning_Day-17');
 
+// Events & Event object 
+
 // document.getElementById("heading").addEventListener("click") 
 // document.getElementById("heading").addEventListener("click", function(){
 //     console.log("You have clicked the heading");
 
-// }
+// });
+
+// How to redirect a webpage in your code
+
+document.getElementById("heading").addEventListener("click", function() {
+    console.log("You have clicked the heading");
+    location.href = '//facebook.com'
+});
+//  How to printEvent object(e)
 
 document.getElementById("heading").addEventListener("click", function(e) {
     let variable;
@@ -21,3 +31,21 @@ document.getElementById("heading").addEventListener("click", function(e) {
     console.log(variable);
     // location.href = '//facebook.com'
 });
+
+document.getElementById("heading").addEventListener('mouseover', function(e) {
+    let variable;
+    console.log("You have clicked the heading");
+    variable = e.target;
+    variable = e.target.className;
+    variable = Array.from(e.target.classList);
+
+    variable = e.target.id;
+    variable = e.offsetX;
+    variable = e.offsetY;
+    variable = e.clientX;
+    variable = e.clientY;
+    console.log(variable);
+    // location.href = '//facebook.com'
+});
+
+console.log('Happy_Learning_JavaScript');
