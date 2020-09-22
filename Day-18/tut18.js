48dbb003f1f20c141c53738b966d80958e48f75b
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-22 16:20:45 +0530

    first commit

--- Day-18/tut18.js
+++ Day-18/tut18.js
@@ -3,40 +3,40 @@ console.log('JavaScript_Learning_Day-18');
 // More on JavaScript Events
 
 
-// let btn = document.getElementById('btn');
-// btn.addEventListener('click', func1);
-// // btn.addEventListener('mousedown', func3);
-// btn.addEventListener('dblclick', func2);
+let btn = document.getElementById('btn');
+btn.addEventListener('click', func1);
+// btn.addEventListener('mousedown', func3);
+btn.addEventListener('dblclick', func2);
 
-// function func1(e) {
-//     console.log("Thanks", e);
-//     e.preventDefault();
-// }
+function func1(e) {
+    console.log("Thanks", e);
+    e.preventDefault();
+}
 
-// function func2(e) {
-//     console.log("Thanks its a double click", e);
-//     e.preventDefault();
-// }
+function func2(e) {
+    console.log("Thanks its a double click", e);
+    e.preventDefault();
+}
 
-// function func3(e) {
-//     console.log("Thanks its a mouse down ", e);
-//     e.preventDefault();
-// }
-
-
-// document.querySelector('.no').addEventListener('mouseenter', function(){
+function func3(e) {
+    console.log("Thanks its a mouse down ", e);
+    e.preventDefault();
+}
 
 
-//     console.log('You entered no')
-// })
+document.querySelector('.no').addEventListener('mouseenter', function() {
 
-// document.querySelector('.no').addEventListener('mouseleave', function(){
-//     console.log('You exited no')
-// })
 
-// document.querySelector('.no').addEventListener('mousemove', function(){
-//     console.log('You triggered mouse move event');
-// })
+    console.log('You entered no')
+})
+
+document.querySelector('.no').addEventListener('mouseleave', function() {
+    console.log('You exited no')
+})
+
+document.querySelector('.no').addEventListener('mousemove', function() {
+    console.log('You triggered mouse move event');
+})
 
 document.querySelector('.container').addEventListener('mousemove', function(e) {
     console.log(e.offsetX, e.offsetY);
