443b35442dee752112a4629d94589ae2667877aa
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-22 16:12:30 +0530

    first commit

---
+++ Day-18/tut18.js
@@ -0,0 +1,35 @@
+console.log('This is tut 18');
+// let btn = document.getElementById('btn');
+// btn.addEventListener('click', func1);
+// // btn.addEventListener('mousedown', func3);
+// btn.addEventListener('dblclick', func2);
+
+// function func1(e) {
+//     console.log("Thanks", e);
+//     e.preventDefault();
+// }
+
+// function func2(e) {
+//     console.log("Thanks its a double click", e);
+//     e.preventDefault();
+// }
+
+// function func3(e) {
+//     console.log("Thanks its a mouse down ", e);
+//     e.preventDefault();
+// }
+// document.querySelector('.no').addEventListener('mouseenter', function(){
+
+
+//     console.log('You entered no')
+// })
+
+// document.querySelector('.no').addEventListener('mouseleave', function(){
+//     console.log('You exited no')
+// })
+
+document.querySelector('.container').addEventListener('mousemove', function(e) {
+    console.log(e.offsetX, e.offsetY);
+    document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetX},154)`;
+    console.log('You triggered mouse move event')
+})
