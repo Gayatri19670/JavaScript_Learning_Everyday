f197c1f833227604203af77ada58e90f555f0358
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-23 18:31:50 +0530

    first commit

--- Day-17/tut17.js
+++ Day-17/tut17.js
@@ -2,6 +2,26 @@ console.log('JavaScript_Learning_Day-17');
 
 // Events & Event object in JavaScript
 
+/*     ****Event Methods & properties ****
+bubbles	 == Returns whether or not a specific event is a bubbling event
+cancelBubble ==	Sets or returns whether the event should propagate up the hierarchy or not
+cancelable ==	Returns whether or not an event can have its default action prevented
+composed ==	Returns whether the event is composed or not
+createEvent() ==	Creates a new event
+composedPath()==	Returns the event's path
+currentTarget ==	Returns the element whose event listeners triggered the event
+defaultPrevented ==	Returns whether or not the preventDefault() method was called for the event
+eventPhase  ==	Returns which phase of the event flow is currently being evaluated
+isTrusted ==	Returns whether or not an event is trusted
+preventDefault() ===	Cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur
+stopImmediatePropagation() ===	Prevents other listeners of the same event from being called
+stopPropagation() ===	Prevents further propagation of an event during event flow
+target ==	Returns the element that triggered the event
+timeStamp ===	Returns the time (in milliseconds relative to the epoch) at which the event was created
+type  ===	Returns the name of the event
+*/
+
+
 document.getElementById("heading").addEventListener("click")
 document.getElementById("heading").addEventListener("click", function() {
     console.log("You have clicked the heading");
