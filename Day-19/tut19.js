2650378f7d0385093b92b922391657446e241fe8
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-23 19:35:40 +0530

    first commit

---
+++ Day-19/tut19.js
@@ -0,0 +1,26 @@
+console.log('JavaScript_Learning_Day-17');
+
+// localStorage == Key & Value
+// SessionStorage == Closing the browser, all the data gets lit.
+
+let impArray = ['adrak', 'pyaz', 'bhindi'];
+
+// // Add a key-value pair inside local Storage
+// localStorage.setItem('Name', 'Harry');
+// localStorage.setItem('Name2', 'Rohan');
+// localStorage.setItem('Sabzi', JSON.stringify(impArray));
+
+// Clears the entire local storage
+// localStorage.clear();
+
+// Clear a particular key-value pair
+// localStorage.removeItem('Name2');
+
+// Retrieve an item from the local Storage
+let name = localStorage.getItem('Name');
+name = JSON.parse(localStorage.getItem('Sabzi'));
+console.log(name)
+
+// sessionStorage.setItem('sessionName', 'sHarry');
+// sessionStorage.setItem('sessionName2', 'sRohan');
+// sessionStorage.setItem('sessionSabzi', JSON.stringify(impArray));
