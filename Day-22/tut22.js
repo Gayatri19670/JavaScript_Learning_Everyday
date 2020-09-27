1bec6aca6476ac6705224e5ebf2a81ba93edd24e
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-27 13:37:13 +0530

    frist Commit

---
+++ Day-22/tut22.js
@@ -0,0 +1,80 @@
+console.log('JavaScript_Learning_Day-22');
+
+// Math Object In JavaScript
+
+/**
+ *Math Object Methods
+Method	              Description
+abs(x)  	        Returns the absolute value of x
+acos(x)	            Returns the arccosine of x, in radians
+acosh(x)       	    Returns the hyperbolic arccosine of x
+asin(x)	         	Returns the arcsine of x, in radians
+asinh(x)	        Returns the hyperbolic arcsine of x
+atan(x)	    Returns the arctangent of x as a numeric value between -PI/2 and PI/2 radians
+atan2(y, x)	Returns the arctangent of the quotient of its arguments
+atanh(x)	Returns the hyperbolic arctangent of x
+cbrt(x)	Returns the cubic root of x
+ceil(x)	Returns x, rounded upwards to the nearest integer
+cos(x)	Returns the cosine of x (x is in radians)
+cosh(x)	Returns the hyperbolic cosine of x
+exp(x)	Returns the value of Ex
+floor(x)	Returns x, rounded downwards to the nearest integer
+log(x)	Returns the natural logarithm (base E) of x
+max(x, y, z, ..., n)	Returns the number with the highest value
+min(x, y, z, ..., n)	Returns the number with the lowest value
+pow(x, y)	Returns the value of x to the power of y
+random()	Returns a random number between 0 and 1
+round(x)	Rounds x to the nearest integer
+sin(x)	Returns the sine of x (x is in radians)
+sinh(x)	Returns the hyperbolic sine of x
+sqrt(x)	Returns the square root of x
+tan(x)	Returns the tangent of an angle
+tanh(x)	Returns the hyperbolic tangent of a number
+trunc(x)	Returns the integer part of a number (x)
+
+ */
+
+let x = 4;
+let y = 5;
+let z;
+console.log(z);
+z = Math;
+console.log(z);
+z = Math.PI;
+console.log(z);
+z = Math.E;
+console.log(z);
+z = Math.round(5.6);
+console.log(z);
+z = Math.round(5.3);
+console.log(z);
+z = Math.ceil(5.6);
+console.log(z);
+z = Math.ceil(5.2);
+console.log(z);
+z = Math.floor(-5.6);
+console.log(z);
+z = Math.floor(5.6);
+console.log(z);
+z = Math.abs(-5.6);
+console.log(z);
+z = Math.abs(5.6);
+console.log(z);
+z = Math.sqrt(5);
+console.log(z);
+z = Math.pow(2, 6);
+console.log(z);
+z = Math.min(8, 7, 9, 6, 3, 2, 4, 5, 6, );
+console.log(z);
+z = Math.max(8, 7, 9, 6, 3, 2, 4, 5, 6, );
+console.log(z);
+z = Math.random();
+console.log(z);
+z = 100 * Math.random();
+console.log(z);
+z = (50 + (100 - 50) * Math.random());
+console.log(z);
+z = Math.ceil(50 + (100 - 50) * Math.random());
+console.log(z);
+
+console.log('Happy_Learning_JavaScript');
