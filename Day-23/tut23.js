e691e7e9926b2ca19ac9b3d54fa254ac1525e329
Gayatri19670 <samirgupta19670@gmail.com>
2020-09-29 07:45:51 +0530

    first commit

---
+++ Day-23/tut23.js
@@ -0,0 +1,63 @@
+console.log("JavaScript_Learning_Day-24");
+
+// Date Object : Date & Time In JavaScript
+
+let today = new Date();
+console.log(today);
+console.log(typeof today);
+
+let otherDate = new Date('08-4-2003 04:54:30');
+console.log(otherDate);
+otherDat = new Date('June 15 1999');
+console.log(otherDate);
+otherDat = new Date('09/15/1999');
+console.log(otherDate);
+
+let a;
+a = otherDate.getDay();
+console.log(otherDate);
+a = otherDate.getDate();
+console.log(otherDate);
+a = otherDate.getMinutes();
+console.log(otherDate);
+a = otherDate.getHours();
+console.log(otherDate);
+a = otherDate.getSeconds();
+console.log(otherDate);
+a = otherDate.getMilliseconds();
+console.log(otherDate);
+a = otherDate.getTime();
+console.log(otherDate);
+a = otherDate.getTimezoneOffset();
+console.log(otherDate);
+
+
+a = otherDate.getUTCDate();
+console.log(otherDate);
+a = otherDate.getUTCDay();
+console.log(otherDate);
+a = otherDate.getUTCFullYear();
+console.log(otherDate);
+a = otherDate.getUTCMonth();
+console.log(otherDate);
+a = otherDate.getUTCHours();
+console.log(otherDate);
+a = otherDate.getUTCMinutes();
+console.log(otherDate);
+a = otherDate.getUTCSeconds();
+console.log(otherDate);
+a = otherDate.getUTCMilliseconds();
+console.log(otherDate);
+
+a = otherDate.setDate(1);
+console.log(otherDate);
+a = otherDate.setMonth(2);
+console.log(otherDate);
+a = otherDate.setMinutes(3);
+console.log(otherDate);
+a = otherDate.setFullYear(4);
+console.log(otherDate);
+a = otherDate.setHours(5);
+console.log(otherDate);
+a = otherDate.setSeconds(6);
+console.log(otherDate);
