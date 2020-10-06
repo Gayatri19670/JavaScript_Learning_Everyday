d4b5780467303dd493319917b6d117ce83818d51
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-06 23:08:20 +0530

    first commit

---
+++ Day-29/tut29.js
@@ -0,0 +1,38 @@
+console.log("JavaScript_Learning_Day-29")
+
+// Library Class Implementation : Exercise-04
+
+/* 
+create a class library and implement the following:
+constructor must take the book list as an argument
+getBookList()
+issueBook(bookname, user)
+returnBook(bookname)
+*/
+
+// Solution :
+
+class Library {
+    constructor(bookList) {
+        this.bookList = bookList;
+        this.issuedBooks = {};
+    }
+
+    getBookList() {
+        this.bookList.forEach(element => {
+            console.log(element);
+        });
+    }
+
+    issueBook(bookname, user) {
+        if (this.issuedBooks[bookname] == undefined) {
+            this.issuedBooks[bookname] = user;
+        } else {
+            console.log("This book is already issued!");
+        }
+    }
+
+    returnBook(bookname) {
+        delete this.issuedBooks[bookname];
+    }
+}
