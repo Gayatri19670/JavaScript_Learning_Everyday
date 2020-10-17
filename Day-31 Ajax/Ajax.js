affb12983458a1bf2393d04e37a4ad0db0d200c5
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-17 15:53:04 +0530

    first commit

--- Day-31 Ajax/Ajax.js
+++ Day-31 Ajax/Ajax.js
@@ -2,38 +2,87 @@ console.log("JavaScript_Learning_Day-30");
 
 // AJAX: Asynchronous JavaScript and XML
 
+
+
+
 let fetchBtn = document.getElementById('fetchBtn');
 fetchBtn.addEventListener('click', buttonClickHandler)
 
 function buttonClickHandler() {
     console.log('You have clicked the fetchBtn');
 
-    //Instantiate an xhr object
-
+    // Instantiate an xhr object
     const xhr = new XMLHttpRequest();
 
-    // Open the objects
-    xhr.open('GET', 'harry.txt', true);
+    // Open the object
+    // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
 
-    // what to do on ProgressEvent(optional)
+    // USE THIS FOR POST REQUEST
+    xhr.open('POST', 'http://dummy.restapiexample.com/api/v1/create', true);
+    xhr.getResponseHeader('Content-type', 'application/json');
+
+
+    // What to do on progress (optional)
     xhr.onprogress = function() {
-        console.log('on progress');
+        console.log('On progress');
     }
 
-    xhr.onreadystatechange = function() {
-            console.log('Ready state is', xhr.readyState);
-        }
-        // what to do response is ready
-    xhr.onload = function() {
-            if (this.status === 200) {
-                console.log('this.responseText');
-            } else {
-                console.log('Some error occured');
-            }
 
+    // xhr.onreadystatechange = function () {
+    //     console.log('ready state is ', xhr.readyState);
+
+    // }
+
+    // What to do when response is ready
+    xhr.onload = function() {
+        if (this.status === 200) {
+
+            console.log(this.responseText)
+        } else {
+            console.log("Some error occured")
         }
-        // send the request
+    }
+
+    // send the request
+    params = `{"name":"test34sad545","salary":"123","age":"23"}`;
+    xhr.send(params);
+
+    console.log("We are done!");
+
+}
+
+let popBtn = document.getElementById('popBtn');
+popBtn.addEventListener('click', popHandler);
+
+function popHandler() {
+    console.log('You have clicked the pop handler');
+
+    // Instantiate an xhr object
+    const xhr = new XMLHttpRequest();
+
+    // Open the object
+    xhr.open('GET', 'harry.txt', true);
+
+
+    // What to do when response is ready
+    xhr.onload = function() {
+        if (this.status === 200) {
+            let obj = JSON.parse(this.responseText);
+            console.log(obj);
+            let list = document.getElementById('list');
+            str = "";
+            for (key in obj) {
+                str += `<li>${obj[key].employee_name} </li>`;
+            }
+            list.innerHTML = str;
+        } else {
+            console.log("Some error occured")
+        }
+    }
+
+    // send the request
     xhr.send();
+    console.log("We are done fetching employees!");
 
 }
 console.log('Happy_Learning_JavaScript');
