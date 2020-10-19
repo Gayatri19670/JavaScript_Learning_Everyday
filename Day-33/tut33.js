6fcb3ef37ab06e3b6ce58dab81f549296ba0ca82
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-19 16:46:16 +0530

    first commit

---
+++ Day-33/tut33.js
@@ -0,0 +1,33 @@
+// Pretent that this response is coming from the server
+
+const students = [
+    { name: "Samir", subject: "JavaScript" },
+    { name: "Alok", subject: "Machin Learning" }
+]
+
+
+
+
+
+
+
+function enrollStudent(student, callback) {
+    setTimeout(function() {
+        students.push(student);
+        console.log('Student Has been enrolled');
+        callback();
+
+    }, 3000);
+
+}
+
+Function getStudents() {
+    setTimeout(function() {
+        let str = "";
+        students.forEach(function(student) {
+            str += `<li> ${student.name}</li>`
+        });
+        document.getElementById('students').innerHTML = str;
+        console.log('Student have been fetched');
+    }, 1000);
+}
