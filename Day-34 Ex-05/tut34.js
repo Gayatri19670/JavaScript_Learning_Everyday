b996ce8aba95fa5906fbcec2bea2b1e8e372bb6d
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-20 17:23:44 +0530

    first commit

--- Day-34 Ex-05/tut34.js
+++ Day-34 Ex-05/tut34.js
@@ -1,6 +1,8 @@
 console.log("JavaScript_Learning_Day-34");
 
-// Using a Dictionary API Exercise 5
+//*****  Exercise-05  *****/
+
+// Using a Dictionary API 
 
 // You have pretend to use a word api which will contain an object and you hve to print definition from all the results of that word api.
 // YOu have to take input from an input tag.
