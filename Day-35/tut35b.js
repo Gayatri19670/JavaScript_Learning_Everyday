9ab7c53c5eda8ac7a242caa5707200c0234c565f
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-21 11:13:48 +0530

    first commit

--- Day-35/tut35b.js
+++ Day-35/tut35b.js
@@ -1,1 +1,36 @@
-console.log("JavaScript_Learning_Day-25")
+console.log("JavaScript_Learning_Day-35b");
+
+// Pretent that this response is coming from the server
+
+
+const students = [
+    { name: "Samir", subject: "JavaScript" },
+    { name: "Alok", subject: "Machin Learning" }
+]
+
+function enrollStudent(student, callback) {
+    setTimeout(function() {
+        students.push(student);
+        console.log('Student Has been enrolled');
+        callback();
+
+    }, 3000);
+
+}
+
+function getStudents() {
+    setTimeout(function() {
+        let str = "";
+        students.forEach(function(student) {
+            str += `<li> ${student.name}</li>`
+        });
+        document.getElementById('students').innerHTML = str;
+        console.log('Student have been fetched');
+    }, 1000);
+}
+
+let newStudent = { name: "Manjay", subject: "Microprocessor" };
+enrollStudent(newStudent);
+getStudents();
+
+console.log('Happy_Learning_JavaScript');
