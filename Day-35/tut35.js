ae9132263d88163944e95a6e45d798a05cf2d0cf
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-21 10:43:23 +0530

    first commit

--- Day-35/tut35.js
+++ Day-35/tut35.js
@@ -1,1 +1,28 @@
 console.log('')
+
+
+/*
+    Promise-
+    -Resolve
+    -Reject
+    -pending
+*/
+
+function func1() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            const error = true;
+            if (!error) {
+                console.log('Function: Your promise has been resolved');
+                resolve();
+            } else {
+                console.log('Function: Your promise has not been resolved')
+                reject('Sorry not fullfilled');
+            }
+        }, 2000);
+    })
+}
+
+func1().then(function() {
+
+})
