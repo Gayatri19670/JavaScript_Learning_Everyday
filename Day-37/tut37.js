c95772c0e784f0331e51d0f56e4df0524a21f89d
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-23 16:44:52 +0530

    first commit

--- Day-37/tut37.js
+++ Day-37/tut37.js
@@ -2,3 +2,12 @@ console.log('JavaScript_Learning_Day-37');
 
 
 // Arrow functions in Javascript
+const samir = function() {
+    console.log('this is the best person ever');
+}
+samir();
+
+const samir = () => {
+    console.log('this is the best person ever');
+}
+samir();
