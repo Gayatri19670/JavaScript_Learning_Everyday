d5dc96e52281b56bb53c1c8e586c7a8440b3e996
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-24 14:36:09 +0530

    first commit

--- Day-37/tut37.js
+++ Day-37/tut37.js
@@ -11,32 +11,33 @@ console.log('JavaScript_Learning_Day-37');
 // samir();
 
 // Converting it to an arrow function
-const samir = () => {
-    console.log('this is the best person ever');
-}
-samir();
+// const samir = () => {
+//     console.log('this is the best person ever');
+// }
+// samir();
 
 
 // function returning something
 
 // const greet = function() {
-//     return 'Good Morning'
+//         return "Good Morning";
 
-// }
-// greet()
+//     }
+// greet();  give error
 
+// console.log(greet());
 // one liners do not require braces/return
 // one line will automatically return
 
 // const greet = () => 'Good Morning';
-
+// console.log(greet());
 // const greet = () => ({ name: "Samir" });
-
+// console.log(greet());
 // Single parameters do not need parenthesis
 // but you will have to put parenthesis if there are multiple parameters
 
-// const greet = name => 'Good Morning' + name + ending;
+const greet = name => "Good Morning" + name + ending;
 
-// console.log('greet');
+console.log(greet('Samir'));
 
 // console.log('Happy Learning JavaScript');
