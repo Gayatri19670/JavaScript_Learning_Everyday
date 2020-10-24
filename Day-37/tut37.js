e87c8dc90738452914351ef24458abceb46a83b3
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-24 14:37:46 +0530

    first commit

--- Day-37/tut37.js
+++ Day-37/tut37.js
@@ -1,38 +1,40 @@
 console.log('JavaScript_Learning_Day-37');
 
-
 // Arrow functions in Javascript
 
 // Creating a regular function
 
-// const samir = function() {
-//     console.log('this is the best person ever');
-// }
-// samir();
+const samir = function() {
+    console.log('this is the best person ever');
+}
+samir();
 
 // Converting it to an arrow function
-// const samir = () => {
-//     console.log('this is the best person ever');
-// }
-// samir();
+const samir = () => {
+    console.log('this is the best person ever');
+}
+samir();
 
 
 // function returning something
 
-// const greet = function() {
-//         return "Good Morning";
+const greet = function() {
+        return "Good Morning";
 
-//     }
-// greet();  give error
+    }
+    // greet();  give error
 
-// console.log(greet());
+console.log(greet());
+
 // one liners do not require braces/return
 // one line will automatically return
 
-// const greet = () => 'Good Morning';
-// console.log(greet());
-// const greet = () => ({ name: "Samir" });
-// console.log(greet());
+const greet = () => 'Good Morning';
+console.log(greet());
+
+const greet = () => ({ name: "Samir" });
+console.log(greet());
+
 // Single parameters do not need parenthesis
 // but you will have to put parenthesis if there are multiple parameters
 
@@ -40,4 +42,4 @@ const greet = name => "Good Morning" + name + ending;
 
 console.log(greet('Samir'));
 
-// console.log('Happy Learning JavaScript');
+console.log('Happy Learning JavaScript');
